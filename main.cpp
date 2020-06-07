621abdc98ab258d8e5362e62e063f9a50888e702
SSRuzibeil <250289173@qq.com>
2020-06-07 23:46:25 +0800

    mod test by ruzibeil

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@ const int faceHeight = faceWidth;
 
 // Try to set the camera resolution. Note that this only works for some cameras on
 // some computers and only for some drivers, so don't rely on it to work!
-const int DESIRED_CAMERA_WIDTH = 640;
+const int DESIRED_CAMERA_WIDTH = 640; // test 640
 const int DESIRED_CAMERA_HEIGHT = 480;
 
 // Parameters controlling how often to keep new faces when collecting them. Otherwise, the training set could look to similar to each other!
